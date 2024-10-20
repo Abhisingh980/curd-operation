82b2a9681a43d6742ada805779c11387e3e366f6
Abhisingh980 <kr.abhinesh147@gmail.com>
2024-10-20 13:42:35 +0530

    add all html files and base html and nav bar

--- curd_Django/CURD/CURD/settings.py
+++ curd_Django/CURD/CURD/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'livereload',
+    'rest_framework',
 ]
 
 EXTRA_APPS = [
@@ -124,6 +125,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [BASE_DIR / 'static']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
